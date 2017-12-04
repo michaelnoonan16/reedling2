b7bbb073bfc9b1aca889be3c32e6b7a7731cb93d
Michael Noonan <michaelnoonan16@gmail.com>
2017-12-04 23:12:03 +0000

    connection string

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://postgres:postgres@localhost:5432';
+var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://yabe:yabePassword@10.131.34.154:5432';
 connectionString+='/yabe';
 module.exports = connectionString;
