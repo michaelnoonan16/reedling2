1e84bc4142052c88bb56886f8431f1805e49fb5f
Michael Noonan <michaelnoonan16@gmail.com>
2018-01-10 22:14:53 +0000

    db connection

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 //var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://postgres:postgres@localhost:5432';
-var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://yabe:yabePassword@10.131.34.154:5432';
-connectionString+='/yabe';
+var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://cnemgyqk:zMEFO99GLHsb1cDAUpXHOoTEmfNiLQhX@horton.elephantsql.com:5432';
+connectionString+='/cnemgyqk';
 module.exports = connectionString;
