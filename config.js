bbe986fd43ab107df2816756b0bff19dafc7801c
Michael Noonan <michaelnoonan16@gmail.com>
2018-01-10 22:38:40 +0000

    clear pw

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 //var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://postgres:postgres@localhost:5432';
-var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://cnemgyqk:zMEFO99GLHsb1cDAUpXHOoTEmfNiLQhX@horton.elephantsql.com:5432';
+var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://qqqq:qqqqqq@horton.elephantsql.com:5432';
 connectionString+='/cnemgyqk';
 module.exports = connectionString;
